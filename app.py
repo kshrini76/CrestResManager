88725cfb7fae62d50e3f7c284407aa748f3afaeb
Shrinivas Kulkarni <shrinivas@crestech.co.in>
2022-07-26 17:05:33 +0530

    pyodbc package used for SQL

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
 from flask import Flask,render_template, request
-from flask_mysqldb import MySQL
+import pyodbc
  
 app = Flask(__name__)
  
-app.config['MYSQL_HOST'] = '192.168.1.25'
-app.config['MYSQL_USER'] = 'crest'
-app.config['MYSQL_PASSWORD'] = 'crest@123'
-app.config['MYSQL_DB'] = 'flask'
-app.config['MYSQL_PORT'] = 3308
+server = 'workreportserver.database.windows.net'
+database = 'WorkReportDB'
+username = 'it'
+password = 'ctspl!123'
+driver= '{SQL Server}'
+cnxn_str = 'DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password
  
-mysql = MySQL(app)
  
 @app.route('/')
 def form():    
-    cursor = mysql.connection.cursor()
-    cursor.execute("select * from info_table") 
-    data = cursor.fetchall()
-    cursor.close()
+    with pyodbc.connect(cnxn_str) as conn:
+        with conn.cursor() as cursor:                			
+            cursor.execute(f"SELECT Name, Username FROM Users")
+            data = cursor.fetchall()
+    
     return render_template('form.html', value=data)
  
 @app.route('/login', methods = ['POST', 'GET'])
@@ -25,16 +26,17 @@ def login():
         return ''
      
     if request.method == 'POST':        
-        name = request.form['name']
-        age = request.form['age']
-        cursor = mysql.connection.cursor()
-        
-        cursor.execute(''' INSERT INTO info_table VALUES(%s,%s)''',(name,age))
-        mysql.connection.commit()
+##        name = request.form['name']
+##        age = request.form['age']
+##        cursor = mysql.connection.cursor()
+##        
+##        cursor.execute(''' INSERT INTO info_table VALUES(%s,%s)''',(name,age))
+##        mysql.connection.commit()
                         
-        cursor.execute("select * from info_table") 
-        data = cursor.fetchall()
-        cursor.close()
+        with pyodbc.connect(cnxn_str) as conn:
+            with conn.cursor() as cursor:                			
+                cursor.execute(f"SELECT Name, Username FROM Users")
+                data = cursor.fetchall()
         
         return render_template('form.html', value=data)
 
